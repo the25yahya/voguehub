0bdb1a628f306e3a3734c2179bca09721c2f6d26
the25yahya <114318659+the25yahya@users.noreply.github.com>
2024-01-18 15:15:19 +0100

    starting

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { BrowserRouter, BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { ChakraProvider } from '@chakra-ui/react';
+import { Home} from './Pages/index.jsx';
+import Navbar from './Components/Navbar';
+import Footer from './Components/Footer';
+
 
 const App = () => {
   return (
-    <div className='underline'>App</div>
+    <ChakraProvider>
+      <BrowserRouter>
+       <Navbar />
+       <Routes>
+        <Route path='/' element = { <Home /> } />
+       </Routes>
+       <Footer />
+      </BrowserRouter>
+    </ChakraProvider>
   )
 }
 
