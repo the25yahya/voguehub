d79fcd002580717495277bccdf9a7708549802da
the25yahya <114318659+the25yahya@users.noreply.github.com>
2024-02-15 18:48:17 +0100

    minor change

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
+import { useStateContext } from '../Contexts/ContextProvider';
+import { FiAlertCircle } from "react-icons/fi";
+import Navbar from '../Components/Navbar';
+import { FaAngleRight } from "react-icons/fa";
+import { Link } from 'react-router-dom';
 
 const Cart = () => {
-  return (
-    <div>Cart</div>
-  )
+  const {cartItems} = useStateContext();
+  if (cartItems.length === 0){
+    return (
+      <div className='flex flex-col items-center'>
+        <Navbar 
+         navbarClasses='fixed w-full z-40 text-white flex justify-between items-center px-8 py-5 border-transparent border-b transition bg-black'
+        />
+        <div className='mt-24 w-full px-20'>
+        <p className='flex items-center'>
+         <span className='flex items-center'>Home<FaAngleRight/></span>
+         <span className='flex items-center'>Shop<FaAngleRight/></span>
+         <span>Cart</span>
+        </p>
+        <div className='flex items-center border border-black bg-gray-100 px-5 py-2 mb-5 ml-20 mt-12'>
+         <div className='mr-2'><FiAlertCircle /></div>
+         <p>Your Cart is currently empty</p>
+        </div>
+        <Link to='/'><button className='bg-black w-200 text-white px-4 py-3 text-sm ml-20 transition hover:bg-transparent hover:text-black'>RETURN TO SHOP</button></Link>
+        </div>
+      </div>
+    )
+  } else if (cartItems.length > 0){
+    return (
+      <div>working</div>
+    )
+  }
 }
 
 export default Cart
