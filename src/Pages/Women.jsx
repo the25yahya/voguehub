7918baf04da03021151a22583dc2e49e3cd61bbb
the25yahya <114318659+the25yahya@users.noreply.github.com>
2024-02-15 16:58:19 +0100

    minor change

--- src/Pages/Women.jsx
+++ src/Pages/Women.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import { useStateContext } from '../Contexts/ContextProvider'
+import Navbar from '../Components/Navbar';
 
 const Women = () => {
   const {WomenProducts} = useStateContext();
 
   return (
-    <div className='pt-50'>
-    <div className='flex pl-28 flex-wrap w-full'>{WomenProducts}</div>
-  </div>
+    <div className='flex flex-col items-center'>
+      <Navbar 
+        navbarClasses='fixed w-full z-40 text-white flex justify-between items-center px-8 py-5 border-transparent border-b transition bg-black'
+      />
+      <div className='flex pl-28 flex-wrap w-full mt-50'>{WomenProducts}</div>
+    </div>
   )
 }
 
